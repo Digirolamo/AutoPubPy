af8bbbdf88e88dbc8259ff4d3cbcd8dbb026e5b0
Digirolamo <CDigirolamo@DigiSpade.com>
2015-11-19 13:58:44 -0500

    Added auth file to repo.

---
+++ autopubpy/auth.py
@@ -0,0 +1,11 @@
+"""This module contains class/functions for
+authorization and authentication.
+
+These are different things.
+    Authentication determines who the client is and whether or not
+        you allow the login.
+
+    Authorization determines what permissions the connected
+        client has. Both in terms of URI's and methods (pub, sub, call, reg).
+
+"""
